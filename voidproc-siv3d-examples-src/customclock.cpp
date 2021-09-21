433a261bcc77a27ecfe0f78e79f8283035db8aa3
voidProc <voidproc@gmail.com>
2021-09-21 23:50:51 +0900

    Fix

--- voidproc-siv3d-examples-src/customclock.cpp
+++ voidproc-siv3d-examples-src/customclock.cpp
@@ -51,42 +51,42 @@ void Main()
 	Font font{ 32, Typeface::Medium };
 
 	// 基準時刻
-	std::shared_ptr<CustomClock> clock = std::make_shared<CustomClock>(StartImmediately::Yes);
+	CustomClock clock{ StartImmediately::Yes };
 
 	// これらを一括で停止したり再開したり、速度を変えたりしたい
-	Stopwatch stopwatch1{ StartImmediately::Yes, clock.get() };
-	VariableSpeedStopwatch stopwatch2{ 0.3, StartImmediately::Yes, clock.get() };
-	VariableSpeedStopwatch stopwatch3{ 1.5, StartImmediately::Yes, clock.get() };
+	Stopwatch stopwatch1{ StartImmediately::Yes, &clock };
+	VariableSpeedStopwatch stopwatch2{ 0.3, StartImmediately::Yes, &clock };
+	VariableSpeedStopwatch stopwatch3{ 1.5, StartImmediately::Yes, &clock };
 
 	while (System::Update())
 	{
 		// [Enter]で停止・再開
 		if (KeyEnter.down())
 		{
-			if (clock->isPaused())
+			if (clock.isPaused())
 			{
-				clock->start();
+				clock.start();
 			}
 			else
 			{
-				clock->pause();
+				clock.pause();
 			}
 		}
 
 		// [↑][↓]でスピードアップ・ダウン
-		const double clockSpeed = clock->getSpeed();
+		const double clockSpeed = clock.getSpeed();
 
 		if (KeyUp.down())
 		{
-			clock->setSpeed(Clamp(clockSpeed + 0.2, 0.2, 4.0));
+			clock.setSpeed(Clamp(clockSpeed + 0.2, 0.2, 4.0));
 		}
 		else if (KeyDown.down())
 		{
-			clock->setSpeed(Clamp(clockSpeed - 0.2, 0.2, 4.0));
+			clock.setSpeed(Clamp(clockSpeed - 0.2, 0.2, 4.0));
 		}
 
-		font(U"Speed: {:.1f}"_fmt(clock->getSpeed())).draw(8, 8);
-		font(clock->isPaused() ? U"(Paused)" : U"").draw(200, 8);
+		font(U"Speed: {:.1f}"_fmt(clock.getSpeed())).draw(8, 8);
+		font(clock.isPaused() ? U"(Paused)" : U"").draw(200, 8);
 		font(U"Stopwatch1: {:.1f}"_fmt(stopwatch1.sF())).draw(8, 24 + (32 + 12) * 1);
 		font(U"Stopwatch2: {:.1f}"_fmt(stopwatch2.sF())).draw(8, 24 + (32 + 12) * 2);
 		font(U"Stopwatch3: {:.1f}"_fmt(stopwatch3.sF())).draw(8, 24 + (32 + 12) * 3);
