72c14c9e179f94b12e89c94f759b3e2bb18ecfea
voidProc <voidproc@gmail.com>
2021-09-23 23:59:48 +0900

    Add example

---
+++ voidproc-siv3d-examples-src/key_mouse_inputs.cpp
@@ -0,0 +1,31 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.0
+
+String commaSeparatedInputNames(const Array<Input>& inputs)
+{
+	return inputs.map([](const auto& x) { return x.name(); })
+		.join(U", ", U"", U"");
+}
+
+void Main()
+{
+	Scene::SetBackground(ColorF{ 0.0, 0.05, 0.1 });
+
+	Font font{ 32, Typeface::Bold };
+
+	while (System::Update())
+	{
+		Array<String> inputNames;
+
+		if (const auto keyInputs = Keyboard::GetAllInputs(); not keyInputs.isEmpty())
+		{
+			inputNames.push_back(commaSeparatedInputNames(keyInputs));
+		}
+
+		if (const auto mouseInputs = Mouse::GetAllInputs(); not mouseInputs.isEmpty())
+		{
+			inputNames.push_back(commaSeparatedInputNames(mouseInputs));
+		}
+
+		font(inputNames.join(U", ", U"", U"")).drawAt(Scene::Center(), ColorF{0.95, 0.98, 1.00});
+	}
+}
