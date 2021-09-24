9424fd720af4e20ca1a1cb8f968eef695d2e6773
voidProc <voidproc@gmail.com>
2021-09-24 21:18:43 +0900

    Add example

---
+++ voidproc-siv3d-examples-src/App/shader/white.hlsl
@@ -0,0 +1,35 @@
+﻿Texture2D    g_texture0 : register(t0);
+SamplerState g_sampler0 : register(s0);
+
+namespace s3d
+{
+	struct PSInput
+	{
+		float4 position	: SV_POSITION;
+		float4 color	: COLOR0;
+		float2 uv		: TEXCOORD0;
+	};
+}
+
+cbuffer PSConstants2D : register(b0)
+{
+	float4 g_colorAdd;
+	float4 g_sdfParam;
+	float4 g_sdfOutlineColor;
+	float4 g_sdfShadowColor;
+	float4 g_internal;
+}
+
+cbuffer WhiteEffectConstants : register(b1)
+{
+	float strength;
+}
+
+float4 PS(s3d::PSInput input) : SV_TARGET
+{
+	float4 srcColor = g_texture0.Sample(g_sampler0, input.uv);
+
+	srcColor.rgb += (float3(1.0, 1.0, 1.0) - srcColor.rgb) * strength;
+
+	return (srcColor * input.color) + g_colorAdd;
+}
---
+++ voidproc-siv3d-examples-src/pixelshader_white.cpp
@@ -0,0 +1,34 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.0
+
+struct WhiteEffectConstants
+{
+	float strength;
+};
+
+void Main()
+{
+	Scene::SetBackground(Color{ 30, 30, 30 });
+
+	const Texture texture{ U"example/siv3d-kun.png" };
+
+	const PixelShader psWhite = HLSL{ U"shader/white.hlsl", U"PS" };
+	if (not psWhite)
+	{
+		throw Error{ U"Failed to load a shader file" };
+	}
+
+	ConstantBuffer<WhiteEffectConstants> cb;
+
+	while (System::Update())
+	{
+		cb->strength = Saturate(Math::Sin(Scene::Time() * 5) * 0.5 + 0.5);
+
+		{
+			Graphics2D::SetPSConstantBuffer(1, cb);
+
+			const ScopedCustomShader2D shader(psWhite);
+
+			texture.scaled(0.75).drawAt(Scene::Center());
+		}
+	}
+}
